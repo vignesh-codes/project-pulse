a4ed73e2238b76c4946e5ef6d17ff5f190da6436
Vignesh <vickymailed@gmail.com>
2023-05-27 23:57:34 +0530

    cleans

--- constants/utils.go
+++ constants/utils.go
@@ -1,62 +1,13 @@
 package constants
 
 import (
-	"fmt"
 	"os"
-	"strconv"
 )
 
 func GetEnvString(key string, defaultValue string) string {
 	value := os.Getenv(key)
 	if len(value) == 0 {
 		value = defaultValue
-	}
-	return value
-}
-
-func GetEnvFormattedString(key string, defaultValue string) string {
-	value := os.Getenv(key)
-	if len(value) == 0 {
-		value = fmt.Sprintf(defaultValue, ENV)
-	}
-	return value
-}
-
-func GetEnvBool(key string, defaultValue bool) bool {
-	envValue := os.Getenv(key)
-	if len(envValue) == 0 {
-		value := defaultValue
-		return value
-	}
-	value, err := strconv.ParseBool(envValue)
-	if err != nil {
-		return defaultValue
-	}
-	return value
-}
-
-func GetEnvInt(key string, defaultValue int) int {
-	envValue := os.Getenv(key)
-	if len(envValue) == 0 {
-		value := defaultValue
-		return value
-	}
-	value, err := strconv.Atoi(envValue)
-	if err != nil {
-		return defaultValue
-	}
-	return value
-}
-
-func GetEnvFloat(key string, defaultValue float64) float64 {
-	envValue := os.Getenv(key)
-	if len(envValue) == 0 {
-		value := defaultValue
-		return value
-	}
-	value, err := strconv.ParseFloat(envValue, 64)
-	if err != nil {
-		return defaultValue
 	}
 	return value
 }
--- logger/constants.go
+++ logger/constants.go
@@ -32,64 +32,35 @@ const (
 var (
 	SUCCESS_RESP = ErrResp{
 		Error_Msg: "SUCCESS",
-		Code:      "N10",
+		Code:      "P10",
 	}
 
 	UNAUTHORIZED_RESP = ErrResp{
 		Error_Msg: "UNAUTHORIZED",
-		Code:      "N20",
+		Code:      "P20",
 	}
 
 	INVALID_REQUEST_PAYLOAD_RESP = ErrResp{
 		Error_Msg: "INVALID_REQUEST",
-		Code:      "N30",
+		Code:      "P30",
 	}
 
 	SERVER_ERROR_RESP = ErrResp{
 		Error_Msg: "SERVER_ERROR",
-		Code:      "N40",
+		Code:      "P40",
 	}
 
 	UNSUPPORTED_MESSAGE_RESP = ErrResp{
 		Error_Msg: "UNSUPPORTED_MESSAGE",
-		Code:      "N000",
-	}
-
-	UID_MISMATCH_RESP = ErrResp{
-		Error_Msg: "UID_MISMATCH",
-		Code:      "N001",
-	}
-
-	USER_LIMIT_REACHED_RESP = ErrResp{
-		Error_Msg: "You have reached max limit of making request, please try again after some time!",
-		Code:      "N003",
+		Code:      "P000",
 	}
 )
 
 const (
-	AwsSession                  = "AWS_SESSION"
-	S3ConnectionError           = "S3_CONNECTION_ERROR"
-	DynamoConnectionError       = "DYNAMODB_CONNECTION_ERROR"
-	RedisError                  = "REDIS_ERROR"
-	DynamoPUTError              = "DYNAMODB:PUT"
-	DynamoGetError              = "DYNAMODB:GET"
-	DynamoUpdateError           = "DYNAMODB:UPDATE"
-	DynamoUpdateExpressionError = "DYNAMODB:UPDATE_EXPRESSION"
-	DynamoUnmarshallError       = "DYNAMODB:UNMARSHALL"
-	DynamoQueryError            = "DYNAMODB:QUERY"
-	DynamoQueryExpressionError  = "DYNAMODB:QUERY_EXPRESSION"
-	DynamoTransactionError      = "DYNAMODB:TRANSACTION_ERROR"
-	SQSError                    = "SQS:ERROR"
-	SQSHandler                  = "SQS_HANDLER"
-	SQSUnknownEvent             = "SQS:UNKNOWN_EVENT"
-	S3FileSaveError             = "S3:FAILED_TO_SAVE_FILE"
-	S3FileDownloadError         = "S3:FAILED_TO_DOWNLOAD_FILE"
-	CSVFileParse                = "CSV:FILE_PARSE_FAILED"
-	StructToMap                 = "GOLANG:STRUCT_TO_MAP"
-	BindJSONtoStruct            = "GOLANG:BINDING_JSON_TO_STRUCT_FAILED"
-	JSONParseError              = "JSON_PARSE_ERROR"
-	JSONUnmarshallError         = "JSON_UNMARSHALL_ERROR"
-	NILValueError               = "NIL_VALUE_ERROR"
-	TransactionHash             = "VICTOR_TRANSACTION_HASH"
-	ErrExternalServiceDown      = "EXTERNAL_SERVICE_DOWN"
+	RedisError          = "REDIS_ERROR"
+	StructToMap         = "GOLANG:STRUCT_TO_MAP"
+	BindJSONtoStruct    = "GOLANG:BINDING_JSON_TO_STRUCT_FAILED"
+	JSONParseError      = "JSON_PARSE_ERROR"
+	JSONUnmarshallError = "JSON_UNMARSHALL_ERROR"
+	NILValueError       = "NIL_VALUE_ERROR"
 )
